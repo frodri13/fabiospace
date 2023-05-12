343860381196c1e47e75116081612035c776708a
Fabio Rodriguez <alessf13@gmail.com>
2023-05-12 22:52:17 +0100

    google auth added

--- next.config.js
+++ next.config.js
@@ -10,7 +10,13 @@ const nextConfig = {
           hostname: 'avatars.githubusercontent.com',
           port: '',
           pathname: '/u/**',
-        }
+        },
+        {
+            protocol: 'https',
+            hostname: 'lh3.googleusercontent.com',
+            port: '',
+            pathname: '/**',
+          }
       ]
     }
   }
