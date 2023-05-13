52af40ea9323626ec72db19ed9c27c42ef568c9c
Fabio Rodriguez <alessf13@gmail.com>
2023-05-13 18:46:47 +0100

    prisma adapter to authjs set up

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,20 @@
 import NextAuth from "next-auth/next";
+import { prisma } from '../../../../lib/pisma';
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import type { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
-import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
     providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID!,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      }),
       GithubProvider({
         clientId: process.env.GITHUB_ID!,
         clientSecret: process.env.GITHUB_SECRET!,
+      }),
+       GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!
       }),
     ]
   };
